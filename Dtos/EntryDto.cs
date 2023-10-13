16b0569a2e2f9eff2c1259837b20b34c3f794929
Lucas Oliveira <d2022004860@unifei.edu.br>
2023-10-13 16:56:10 -0300

    [FIX] - Update Sintax

--- Dtos/EntryDto.cs
+++ Dtos/EntryDto.cs
@@ -4,11 +4,11 @@ public class EntryDto
 {
     public Guid Id { get; init; }
     public DateTime CreatedDate { get; set; }
-    public String? Description { get; set; }
-    public String? Project { get; set; }
-    public String? Manager { get; set; }
-    public String? Worker { get; set; }
+    public string? Description { get; set; }
+    public string? Project { get; set; }
+    public string? Manager { get; set; }
+    public string? Worker { get; set; }
     public Boolean HasOwnerSeen { get; set; }
     public Boolean HasManagerSeen { get; set; }
-    public String[]? Files { get; set; }
+    public string[]? Files { get; set; }
 }
